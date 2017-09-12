89ff85642c331cfdba9b95209110a21e14951963
guilhermesilva <guilhermesilva@NB023659>
2017-09-12 19:11:05 -0300

    Telas de exemplo de quiz

--- AppMeuQuiz/AppMeuQuiz/Views/Questao1View.xaml.cs
+++ AppMeuQuiz/AppMeuQuiz/Views/Questao1View.xaml.cs
@@ -12,9 +12,48 @@ namespace AppMeuQuiz.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Questao1View : ContentPage
     {
+        string Resposta;
+        string Texto;
+
         public Questao1View()
         {
             InitializeComponent();
         }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            switch (((Button)sender).CommandParameter.ToString())
+            {
+                case "1":
+                    Resposta = "Correto";
+                    Texto = "Parabéns! você acertou!";
+                    break;
+
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                    Resposta =
+                        "Errou";
+                    Texto = "Você errou :-(";
+                    break;
+
+                default:
+                    break;
+            }
+
+
+            await ShowMessage(Resposta, Texto, "Ok", async () =>
+                   {
+                       await App.Current.MainPage.Navigation.PushAsync(new MainPage());
+                   });
+        }
+
+        public async Task ShowMessage(string message, string title, string buttonText, Action afterHideCallback)
+        {
+            await DisplayAlert(title, message, buttonText);
+            afterHideCallback?.Invoke();
+        }
+
     }
 }
--- AppMeuQuiz/AppMeuQuiz/Views/Questao2View.xaml.cs
+++ AppMeuQuiz/AppMeuQuiz/Views/Questao2View.xaml.cs
@@ -12,9 +12,46 @@ namespace AppMeuQuiz.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Questao2View : ContentPage
     {
+        string Resposta;
+        string Texto;
+
         public Questao2View()
         {
             InitializeComponent();
+        }
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            switch (((Button)sender).CommandParameter.ToString())
+            {
+                case "1":
+                    Resposta = "Correto";
+                    Texto = "Parabéns! você acertou!";
+                    break;
+
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                    Resposta =
+                        "Errou";
+                    Texto = "Você errou :-(";
+                    break;
+
+                default:
+                    break;
+            }
+
+
+            await ShowMessage(Resposta, Texto, "Ok", async () =>
+            {
+                await App.Current.MainPage.Navigation.PushAsync(new MainPage());
+            });
+        }
+
+        public async Task ShowMessage(string message, string title, string buttonText, Action afterHideCallback)
+        {
+            await DisplayAlert(title, message, buttonText);
+            afterHideCallback?.Invoke();
         }
     }
 }
