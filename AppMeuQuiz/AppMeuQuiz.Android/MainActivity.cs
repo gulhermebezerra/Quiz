7147089d183a50227272d12c4cf26afef2c4084e
guilhermesilva <guilhermesilva@NB023659>
2017-09-19 08:49:49 -0300

    Ajuste no titulo do App

--- AppMeuQuiz/AppMeuQuiz.Android/MainActivity.cs
+++ AppMeuQuiz/AppMeuQuiz.Android/MainActivity.cs
@@ -10,7 +10,7 @@ using Xamarin.Forms;
 
 namespace AppMeuQuiz.Droid
 {
-    [Activity(Label = "AppMeuQuiz", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Quiz", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
